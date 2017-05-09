2c947560b2fd2da67bccb974bd7740eaa201bc26
mvdrijt <michael.van.de.rijt@maxict.nl>
2017-05-09 08:30:46 +0200

    Fixed issue #103

--- src/Artisaninweb/SoapWrapper/Facade.php
+++ src/Artisaninweb/SoapWrapper/Facade.php
@@ -9,6 +9,6 @@ class Facade extends \Illuminate\Support\Facades\Facade
    */
   protected static function getFacadeAccessor()
   {
-    return 'SoapWrapper';
+    return SoapWrapper::class;
   }
 }
