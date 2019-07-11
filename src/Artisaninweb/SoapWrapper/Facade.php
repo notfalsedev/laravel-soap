685ddf37748773d23bfa322fecd881bf79f3498a
Fernando Kupper Cardoso <fernando@kupper.com.br>
2019-07-11 13:02:03 +0200

    Include @method annotation to help IDEs

--- src/Artisaninweb/SoapWrapper/Facade.php
+++ src/Artisaninweb/SoapWrapper/Facade.php
@@ -2,6 +2,13 @@
 
 namespace Artisaninweb\SoapWrapper;
 
+/**
+ * @method static SoapWrapper add(string $name, \Closure $closure) Add a new service to the wrapper
+ * @method static void addByArray(array $services) Add services by array
+ * @method static mixed client(string $name, \Closure $closure) Get the client
+ * @method static mixed call(string $call, array $data, array $options) A easy access call method
+ * @method static bool has(string $name) Check if wrapper has service
+ */
 class Facade extends \Illuminate\Support\Facades\Facade
 {
     /**
